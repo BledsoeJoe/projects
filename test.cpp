05d605b1c89c40403a543fefb92ff8d6b085fec1
Joe Bledsoe <joseph.b.bledsoe@gmail.com>
2021-12-08 17:07:12 -0500

    testing a modification

--- test.cpp
+++ test.cpp
@@ -1,8 +1,8 @@
-#test file for attempting some basic pulls and commits
+//test file for attempting some basic pulls and commits
 #include<iostream>
 using namespace std;
 
 int main(){
-  cout<<"Success, you've made a github!";
+  cout<<"Success, you've made a github!"<<endl;
   return 0;
 }
